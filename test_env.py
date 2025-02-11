b476455f96755363042731d4f049727e8777d114
tnohrer <110556871+tnohrer@users.noreply.github.com>
2025-02-11 11:55:38 -0500

    Delete test_env.py
    test environment is no longer needed now that variables are being used

--- test_env.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-print("Username:", os.getenv("SALESFORCE_USERNAME"))
-print("Password:", "***" if os.getenv("SALESFORCE_PASSWORD") else "Not found")
-print("Token:", "***" if os.getenv("SALESFORCE_SECURITY_TOKEN") else "Not found")
